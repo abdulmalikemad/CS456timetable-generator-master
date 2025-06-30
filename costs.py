cd0e8d52e746664575a84f03cebb89e9aa91fd5c
Abdulmalik Emad <lyabdoalmalk@gmail.com>
2025-06-30 18:11:15 +0200

    Update costs.py

--- costs.py
+++ costs.py
@@ -1,102 +1,71 @@
 def subjects_order_cost(subjects_order):
-    """
-    Calculates percentage of soft constraints - order of subjects (P, V, L).
-    :param subjects_order: dictionary where key = (name of the subject, index of the group), value = [int, int, int]
-    where ints represent start times (row in matrix) for types of classes P, V and L respectively. If start time is -1
-    it means that that subject does not have that type of class.
-    :return: percentage of satisfied constraints
-    """
-    # number of subjects not in right order
     cost = 0
-    # number of all orders of subjects
     total = 0
 
     for (subject, group_index), times in subjects_order.items():
-
         if times[0] != -1 and times[1] != -1:
             total += 1
-            # P after V
             if times[0] > times[1]:
                 cost += 1
 
         if times[0] != -1 and times[2] != -1:
             total += 1
-            # P after L
             if times[0] > times[2]:
                 cost += 1
 
         if times[1] != -1 and times[2] != -1:
             total += 1
-            # V after L
             if times[1] > times[2]:
                 cost += 1
 
-    # print(cost, total)
+    if total == 0:
+        return 100  # ما فيش ولا حالة تحتاج تقييم → اعتبرها 100% صح
+
     return 100 * (total - cost) / total
 
 
+
 def empty_space_groups_cost(groups_empty_space):
-    """
-    Calculates total empty space of all groups for week, maximum empty space in day and average empty space for whole
-    week per group.
-    :param groups_empty_space: dictionary where key = group index, values = list of rows where it is in
-    :return: total cost, maximum per day, average cost
-    """
-    # total empty space of all groups for the whole week
     cost = 0
-    # max empty space in one day for some group
     max_empty = 0
 
     for group_index, times in groups_empty_space.items():
         times.sort()
-        # empty space for each day for current group
-        empty_per_day = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
+        empty_per_day = {i: 0 for i in range(6)}  # 6 أيام
 
         for i in range(1, len(times) - 1):
             a = times[i-1]
             b = times[i]
             diff = b - a
-            # classes are in the same day if their time div 12 is the same
-            if a // 12 == b // 12 and diff > 1:
-                empty_per_day[a // 12] += diff - 1
+            if a // 9 == b // 9 and diff > 1:
+                empty_per_day[a // 9] += diff - 1
                 cost += diff - 1
 
-        # compare current max with empty spaces per day for current group
-        for key, value in empty_per_day.items():
+        for value in empty_per_day.values():
             if max_empty < value:
                 max_empty = value
 
     return cost, max_empty, cost / len(groups_empty_space)
 
 
+
 def empty_space_teachers_cost(teachers_empty_space):
-    """
-    Calculates total empty space of all teachers for week, maximum empty space in day and average empty space for whole
-    week per teacher.
-    :param teachers_empty_space: dictionary where key = name of the teacher, values = list of rows where it is in
-    :return: total cost, maximum per day, average cost
-    """
-    # total empty space of all teachers for the whole week
     cost = 0
-    # max empty space in one day for some teacher
     max_empty = 0
 
     for teacher_name, times in teachers_empty_space.items():
         times.sort()
-        # empty space for each day for current teacher
-        empty_per_day = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
+        empty_per_day = {i: 0 for i in range(6)}  # 6 أيام
 
         for i in range(1, len(times) - 1):
             a = times[i - 1]
             b = times[i]
             diff = b - a
-            # classes are in the same day if their time div 12 is the same
-            if a // 12 == b // 12 and diff > 1:
-                empty_per_day[a // 12] += diff - 1
+            if a // 9 == b // 9 and diff > 1:
+                empty_per_day[a // 9] += diff - 1
                 cost += diff - 1
 
-        # compare current max with empty spaces per day for current teacher
-        for key, value in empty_per_day.items():
+        for value in empty_per_day.values():
             if max_empty < value:
                 max_empty = value
 
@@ -104,11 +73,8 @@ def empty_space_teachers_cost(teachers_empty_space):
 
 
 def free_hour(matrix):
-    """
-    Checks if there is an hour without classes. If so, returns it in format 'day: hour', otherwise -1.
-    """
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
+    hours = [8, 9, 10, 11, 12, 13, 14, 15, 16]
 
     for i in range(len(matrix)):
         exists = True
@@ -118,9 +84,10 @@ def free_hour(matrix):
                 exists = False
 
         if exists:
-            return '{}: {}'.format(days[i // 12], hours[i % 12])
+            return '{}: {}'.format(days[i // 9], hours[i % 9])
 
     return -1
+
 
 
 def hard_constraints_cost(matrix, data):
@@ -207,3 +174,38 @@ def check_hard_constraints(matrix, data):
                                     overlaps += 1
 
     return overlaps
+def get_empty_space_data(matrix, data):
+    teachers_empty_space = {}
+    groups_empty_space = {}
+    subjects_order = {}
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            class_id = matrix[i][j]
+            if class_id is None:
+                continue
+            cls = data.classes[class_id]
+
+            # أوقات المدرسين
+            if cls.teacher not in teachers_empty_space:
+                teachers_empty_space[cls.teacher] = []
+            teachers_empty_space[cls.teacher].append(i)
+
+            # أوقات المجموعات
+            for group in cls.groups:
+                if group not in groups_empty_space:
+                    groups_empty_space[group] = []
+                groups_empty_space[group].append(i)
+
+            # ترتيب الأنواع (P, V, L)
+            key = (cls.subject, list(cls.groups)[0])
+            if key not in subjects_order:
+                subjects_order[key] = [-1, -1, -1]
+            if cls.type == 'P':
+                subjects_order[key][0] = i
+            elif cls.type == 'V':
+                subjects_order[key][1] = i
+            elif cls.type == 'L':
+                subjects_order[key][2] = i
+
+    return teachers_empty_space, groups_empty_space, subjects_order
